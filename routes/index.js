17ec5ec644912e890347cf953fa10bceaee34f1e
jramyasri <jramyasri01@gmail.com>
2022-04-06 18:23:32 -0500

    updated files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+extends layout
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+block content
+  h1= title
+  p Getting familiar with pug #{title}
+  button(type="button", class="btn btn-primary", onclick="moreClaps()") Press Me
+  p 
+  div(id="target") Clap for me 
+  script. 
+    var clap_count=0;
+    function moreClaps(){
+      clap_count+=1;
+      document.getElementById("target").innerHTML="Value is "+clap_count  ;
+    }
